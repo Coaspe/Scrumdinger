d21c7ccbb72f282b7488224c434ef5e38718f437
Coaspe <aspalt85@gmail.com>
2023-01-21 00:56:56 +0900

    Initial Commit

---
+++ Dinger/DingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DingerApp.swift
+//  Dinger
+//
+//  Created by 이우람 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
