cfc2690719d3b51e5104068565bdb39988da1e22
Coaspe <aspalt85@gmail.com>
2023-01-22 00:31:32 +0900

    First

--- Dinger/DingerApp.swift
+++ Dinger/DingerApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct DingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
